81482f2e5d9382fe299de44c801ad9cb05dd600e
lyyzzayo-commits <lyyzzayo@gmail.com>
2026-01-20 22:13:01 +0900

    ㅍㅍ

--- fps project/Assets/Scripts/MonsterSpawner.cs
+++ fps project/Assets/Scripts/MonsterSpawner.cs
@@ -1,25 +1,77 @@
+using Mono.Cecil.Cil;
 using UnityEngine;
+
 
 public sealed class MonsterSpawner : MonoBehaviour
 {
-    [Header("Config")]
-    [SerializeField] private GameObject monsterPrefab;
-    
+   
     [Header("Spawn Points (optional)")]
     [SerializeField] private Transform[] spawnPoints;
 
     [Header("Parent (optional)")]
     [SerializeField] private Transform spawnRoot;
 
+    [Header("Ref")]
+    [SerializeField] private DifficultyDirector director;
+
+    [Header("Spawn Control")]
+    [SerializeField] private MonsterDef monsterDef;
+    [SerializeField] private bool autoSpawn = true;
+
+    private float nextSpawnTime;   
+
+    
+
+    private void OnEnable()
+    {
+        GameSignals.SurvivalTimeUpdated += OnElapsedTimeUpdated;
+    }
+
+    private void OnDisable()
+    {
+        GameSignals.SurvivalTimeUpdated -= OnElapsedTimeUpdated;
+    }
+
+    private void OnElapsedTimeUpdated(float elapsed)
+    {
+
+        if (director == null) return;
+
+        director.elapsedTime = Mathf.Max(0f, elapsed);
+
+        director.difficulty = director.EvaluateDifficulty(director.elapsedTime);
+        director.currentSpawnInterval = director.ComputeSpawnInterval(director.difficulty);
+        director.currentMoveSpeed = director.ComputeSpeed(director.difficulty);
+    }
+
+    private void Update()
+    {
+        if (!autoSpawn) return;
+        if (monsterDef == null) return;
+
+        float interval = 1f;
+        if (director != null)
+            interval = Mathf.Max(0.05f,director.currentSpawnInterval);
+        if(Time.time < nextSpawnTime) return;
+
+        Spawn(monsterDef);
+
+        nextSpawnTime = Time.time + interval;
+    }
+
     public GameObject Spawn(MonsterDef def)
     {
-        if (monsterPrefab == null)
+        if ( def == null)
         {
 #if UNITY_EDITOR
             Debug.LogError("[MonsterSpawner] monsterPrefab is null.");
 #endif
             return null;
         }
+
+        float hp = def.maxHp;
+        float dmg = def.damage;
+        float spd = def.moveSpeed * director.currentMoveSpeed;
 
         Transform parent = spawnRoot != null ? spawnRoot : transform;
         
@@ -29,8 +81,8 @@ public sealed class MonsterSpawner : MonoBehaviour
         if (spawnPoints != null && spawnPoints.Length > 0)
         {
             Transform p = spawnPoints[Random.Range(0,spawnPoints.Length)];
-            pos = parent.position;
-            rot = parent.rotation;
+            pos = p.position;
+            rot = p.rotation;
         }
         else
         {
@@ -42,11 +94,15 @@ public sealed class MonsterSpawner : MonoBehaviour
             def.prefab,
             pos,
             rot,
-            spawnRoot
+            parent
         );
-        var initiallizer = monster.GetComponent<MonsterInitializer>();
-        initiallizer?.Initialize(def);
 
+        
+
+        var stats = monster.GetComponent<MonsterStats>();
+        stats.InitFromDef(hp,dmg,spd);
+        
+        
         return monster;
     } 
 }
